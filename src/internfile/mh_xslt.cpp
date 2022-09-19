5ed1c2772cdaafa5d0f4ae8785225af749409454
Jean Collonvillé <jcollonville@kertel.com>
2022-09-19 16:23:34 +0200

    fix merge

--- src/internfile/mh_xslt.cpp
+++ src/internfile/mh_xslt.cpp
@@ -15,13 +15,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 #include "autoconfig.h"
-#ifdef USE_XALAN
-    #include <sstream>
-    #include <xercesc/dom/DOMDocument.hpp>
-    #include <xercesc/dom/DOMImplementation.hpp>
-    #include <xercesc/framework/URLInputSource.hpp>
-    #include <xalanc/PlatformSupport/URISupport.hpp>
-    #include <xalanc/XercesParserLiaison/XercesDOMSupport.hpp>
 
 #if defined(HAVE_MALLOC_H)
 #include <malloc.h>
@@ -29,29 +22,12 @@
 #include <malloc/malloc.h>
 #endif
 
-    XALAN_USING_XALAN(XSLTInputSource)
-    XALAN_USING_XALAN(XalanTransformer)
-    XALAN_USING_XALAN(XSLTResultTarget)
-    XALAN_USING_XALAN(XalanCompiledStylesheet)
-    XALAN_USING_XERCES(XMLPlatformUtils)
-    #define XSLTStyleSheet const XalanCompiledStylesheet
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <libxslt/transform.h>
+#include <libxslt/xsltInternals.h>
+#include <libxslt/xsltutils.h>
 
-    pthread_once_t  thread_once_control = PTHREAD_ONCE_INIT;                                                                                      
-    void xalan_init() {                                                                               
-        XMLPlatformUtils::Initialize();
-        XalanTransformer::initialize();                                                           
-    }
-
-#else
-    #include <libxml/parser.h>
-    #include <libxml/tree.h>
-    #include <libxslt/transform.h>
-    #include <libxslt/xsltInternals.h>
-    #include <libxslt/xsltutils.h>
-
-    #define XSLTStyleSheet xsltStylesheet
-
-#endif
 #include "cstr.h"
 #include "mh_xslt.h"
 #include "log.h"
@@ -59,10 +35,6 @@
 #include "md5ut.h"
 #include "rclconfig.h"
 #include "readfile.h"
-
-#ifndef _WIN32
-#include "malloc.h"
-#endif
 
 using namespace std;
 
@@ -73,15 +45,6 @@ using namespace std;
 // Probably not:    xmlCleanupParser();
         
 
-<<<<<<< HEAD
-#ifdef USE_XALAN
-    class FileScanXML : public FileScanDo {
-    public:
-        FileScanXML(const string& fn) : m_doc(m_docBuf), m_fn(fn)  {}
-       
-        XSLTInputSource& getDoc() {
-            return m_doc;            
-=======
 class FileScanXML : public FileScanDo {
 public:
     FileScanXML(const string& fn) : m_fn(fn) {}
@@ -96,12 +59,9 @@ public:
 #ifdef HAVE_MALLOC_TRIM
             malloc_trim(0);
 #endif /* HAVE_MALLOC_TRIM */
->>>>>>> upstream/master
         }
+    }
 
-<<<<<<< HEAD
-        virtual bool init(int64_t size, string *) {
-=======
     xmlDocPtr getDoc() {
         int ret;
         if ((ret = xmlParseChunk(ctxt, nullptr, 0, 1))) {
@@ -121,141 +81,57 @@ public:
             LOGERR("FileScanXML: xmlCreatePushParserCtxt failed\n");
             return false;
         } else {
->>>>>>> upstream/master
             return true;
         }
-        
-        virtual bool data(const char *buf, int cnt, string*) {
-            if (0) {
-                string dt(buf, cnt);
-                LOGDEB1("FileScanXML: data: cnt " << cnt << " data " << dt << endl);
-            } else {
-                LOGDEB1("FileScanXML: data: cnt " << cnt << endl);
-            }            
-
-            m_docBuf.write(buf, cnt);
+    }
+    
+    virtual bool data(const char *buf, int cnt, string*) {
+        if (0) {
+            string dt(buf, cnt);
+            LOGDEB1("FileScanXML: data: cnt " << cnt << " data " << dt << endl);
+        } else {
+            LOGDEB1("FileScanXML: data: cnt " << cnt << endl);
+        }            
+        int ret;
+        if ((ret = xmlParseChunk(ctxt, buf, cnt, 0))) {
+            xmlError *error = xmlGetLastError();
+            LOGERR("FileScanXML: xmlParseChunk failed with error " <<
+                   ret << " for [" << buf << "] error " <<
+                   (error ? error->message :
+                    " null return from xmlGetLastError()") << "\n");
+            return false;
+        } else {
+            LOGDEB1("xmlParseChunk ok (sent " << cnt << " bytes)\n");
             return true;
         }
+    }
 
-    private:
-        XSLTInputSource m_doc;
-        stringstream m_docBuf;
-        string m_fn;
-    };
-
-#else
-    class FileScanXML : public FileScanDo {
-    public:
-        FileScanXML(const string& fn) : m_fn(fn) {}
-        virtual ~FileScanXML() {
-            if (ctxt) {
-                xmlFreeParserCtxt(ctxt);
-#ifndef _WIN32
-                malloc_trim(0);
-#endif
-            }
-        }
-
-        xmlDocPtr getDoc() {
-            int ret;
-            if ((ret = xmlParseChunk(ctxt, nullptr, 0, 1))) {
-                xmlError *error = xmlGetLastError();
-                LOGERR("FileScanXML: final xmlParseChunk failed with error " <<
-                    ret << " error: " <<
-                    (error ? error->message :
-                        " null return from xmlGetLastError()") << "\n");
-                return nullptr;
-            }
-            return ctxt->myDoc;
-        }
-
-        virtual bool init(int64_t size, string *) {
-            LOGDEB1("FileScanXML: init: size " << size << endl);
-            ctxt = xmlCreatePushParserCtxt(NULL, NULL, NULL, 0, m_fn.c_str());
-            if (ctxt == nullptr) {
-                LOGERR("FileScanXML: xmlCreatePushParserCtxt failed\n");
-                return false;
-            } else {
-                return true;
-            }
-        }
-        
-        virtual bool data(const char *buf, int cnt, string*) {
-            if (0) {
-                string dt(buf, cnt);
-                LOGDEB1("FileScanXML: data: cnt " << cnt << " data " << dt << endl);
-            } else {
-                LOGDEB1("FileScanXML: data: cnt " << cnt << endl);
-            }            
-            int ret;
-            if ((ret = xmlParseChunk(ctxt, buf, cnt, 0))) {
-                xmlError *error = xmlGetLastError();
-                LOGERR("FileScanXML: xmlParseChunk failed with error " <<
-                    ret << " for [" << buf << "] error " <<
-                    (error ? error->message :
-                        " null return from xmlGetLastError()") << "\n");
-                return false;
-            } else {
-                LOGDEB1("xmlParseChunk ok (sent " << cnt << " bytes)\n");
-                return true;
-            }
-        }
-
-    private:
-        xmlParserCtxtPtr ctxt{nullptr};
-        string m_fn;
-    };
-#endif
+private:
+    xmlParserCtxtPtr ctxt{nullptr};
+    string m_fn;
+};
 
 class MimeHandlerXslt::Internal {
 public:
     Internal(MimeHandlerXslt *_p)
         : p(_p) {}
     ~Internal() {
-<<<<<<< HEAD
-        if (metaOrAllSS) {
-#ifdef USE_XALAN            
-            transformer.destroyStylesheet(metaOrAllSS);
-#else
-            xsltFreeStylesheet(metaOrAllSS);
-#endif            
-        }
-        if (bodySS) {
-#ifdef USE_XALAN
-            transformer.destroyStylesheet(bodySS);
-#else
-            xsltFreeStylesheet(bodySS);
-#endif            
-=======
         for (auto& entry : metaOrAllSS) {
             xsltFreeStylesheet(entry.second);
         }
         for (auto& entry : bodySS) {
             xsltFreeStylesheet(entry.second);
->>>>>>> upstream/master
         }
     }
-    
-    XSLTStyleSheet *prepare_stylesheet(const string& ssnm);
 
+    xsltStylesheet *prepare_stylesheet(const string& ssnm);
     bool process_doc_or_string(bool forpv, const string& fn, const string& data);
     bool apply_stylesheet(
         const string& fn, const string& member, const string& data,
-        XSLTStyleSheet *ssp, string& result, string *md5p);
+        xsltStylesheet *ssp, string& result, string *md5p);
 
     MimeHandlerXslt *p;
     bool ok{false};
-<<<<<<< HEAD
-    string metamember;
-
-#ifdef USE_XALAN
-    XalanTransformer    transformer;
-#endif    
-    XSLTStyleSheet *metaOrAllSS{nullptr};
-    XSLTStyleSheet *bodySS{nullptr};
-
-    string bodymember;
-=======
 
     // Pairs of zip archive member names and style sheet names for the
     // metadata, and map of style sheets refd by their names.
@@ -266,31 +142,25 @@ public:
     // Same for body data
     vector<pair<string,string>> bodyMembers;
     map<string, xsltStylesheet*> bodySS;
->>>>>>> upstream/master
     string result;
     string filtersdir;
 };
 
- 
 MimeHandlerXslt::~MimeHandlerXslt()
 {
     delete m;
 }
-bool MimeHandlerXslt::m_isInit=false;
 
 MimeHandlerXslt::MimeHandlerXslt(RclConfig *cnf, const std::string& id,
                                  const std::vector<std::string>& params)
-    : RecollFilter(cnf, id)
+    : RecollFilter(cnf, id), m(new Internal(this))
 {
-#ifdef USE_XALAN
-    pthread_once(&thread_once_control, xalan_init);
-#else
-    xmlSubstituteEntitiesDefault(0);
-    xmlLoadExtDtdDefaultValue = 0;
-#endif
-    m=new Internal(this);
     LOGDEB("MimeHandlerXslt: params: " << stringsToString(params) << endl);
     m->filtersdir = path_cat(cnf->getDatadir(), "filters");
+
+    xmlSubstituteEntitiesDefault(0);
+    xmlLoadExtDtdDefaultValue = 0;
+
     // params can be "xslt stylesheetall" or
     // "xslt meta/body memberpath stylesheetnm [... ... ...] ...
     if (params.size() == 2) {
@@ -335,23 +205,9 @@ MimeHandlerXslt::MimeHandlerXslt(RclConfig *cnf, const std::string& id,
     }
 }
 
-XSLTStyleSheet *MimeHandlerXslt::Internal::prepare_stylesheet(const string& ssnm)
+xsltStylesheet *MimeHandlerXslt::Internal::prepare_stylesheet(const string& ssnm)
 {
     string ssfn = path_cat(filtersdir, ssnm);
-
-#ifdef USE_XALAN    
-    const XSLTInputSource   stylesheetInputSource(ssfn.c_str());
-    const XalanCompiledStylesheet*      stylesheet = 0;
-
-    if (transformer.compileStylesheet(stylesheetInputSource, stylesheet) != 0)
-    {
-        LOGERR("An error occurred compiling the stylesheet: "
-                << transformer.getLastError()
-                << endl);
-        return nullptr;
-    }
-    return stylesheet;
-#else
     FileScanXML XMLstyle(ssfn);
     string reason;
     if (!file_scan(ssfn, &XMLstyle, &reason)) {
@@ -366,12 +222,11 @@ XSLTStyleSheet *MimeHandlerXslt::Internal::prepare_stylesheet(const string& ssnm
         return nullptr;
     }
     return xsltParseStylesheetDoc(stl);
-#endif    
 }
 
 bool MimeHandlerXslt::Internal::apply_stylesheet(
     const string& fn, const string& member, const string& data,
-    XSLTStyleSheet *ssp, string& result, string *md5p)
+    xsltStylesheet *ssp, string& result, string *md5p)
 {
     FileScanXML XMLdoc(fn);
     string md5, reason;
@@ -395,18 +250,6 @@ bool MimeHandlerXslt::Internal::apply_stylesheet(
                fn << " " << member << " : " << reason << endl);
         return false;
     }
-#ifdef USE_XALAN
-    XSLTInputSource doc=XMLdoc.getDoc();
-    stringstream ss;
-    const XSLTResultTarget  resultTarget(ss);
-
-    int ret = transformer.transform(doc, ssp, resultTarget);
-
-    result=ss.str();
-
-    return ret == 0;
-
-#else
 
     xmlDocPtr doc = XMLdoc.getDoc();
     if (nullptr == doc) {
@@ -427,7 +270,6 @@ bool MimeHandlerXslt::Internal::apply_stylesheet(
     xmlFreeDoc(transformed);
     xmlFreeDoc(doc);
     return true;
-#endif    
 }
 
 bool MimeHandlerXslt::Internal::process_doc_or_string(
